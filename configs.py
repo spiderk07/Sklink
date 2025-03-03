8032d795447f78f95fec340276c7975508ec5899
spiderk07 <130534772+spiderk07@users.noreply.github.com>
2025-03-04 00:09:10 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -2,15 +2,15 @@ import os
 
 class Config:
     # Telegram API credentials
-    API_ID = int(os.environ.get("API_ID", "YOUR_API_ID"))
-    API_HASH = os.environ.get("API_HASH", "YOUR_API_HASH")
+    API_ID = int(os.environ.get("API_ID", "27967371"))
+    API_HASH = os.environ.get("API_HASH", "c8c22d9e427b8589236a6cd94a82a244")
 
     # Bot token
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "YOUR_BOT_TOKEN")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6922466504:AAGKesha1FoDfa52BX32gYqE9V-TLKdpA_Y")
     BOT_SESSION_NAME = os.environ.get("BOT_SESSION_NAME", "SkFilmBot")
 
     # User session string (for searching in channel)
-    USER_SESSION_STRING = os.environ.get("USER_SESSION_STRING", "YOUR_USER_SESSION_STRING")
+    USER_SESSION_STRING = os.environ.get("USER_SESSION_STRING", "BQGqv4sAxvmjqnc2CY4QqnaNZOt-T3F4DYrM0tyX5LnRaPFy64pJaqLekgLtf8bIglqnorz-fRxxMimtM6oZMuekL6fs_4yOhOyTos22QVlRdcGG-qeGbK0mR7YwuWl8EmOj-OBAiqjGv1eYiMpcIhu1vrTCJGb2b2JzYv7CSd43_jC2tTSPne3Qbo_HSSuShWIa287ckUFbtZi10jPmM2KR3YjIzhCWEs5LtSB8gYM_BBpDxaVIzirS4pbOvI0b1rk2H0tiCfI72g0VqM6xmTv3B8PUc_GeZU2zXXl-nT9LjTYlTeFKa_rozq_WoMB-QcmpXg1JUe016Lj8FwBKXZqxOG9LrwAAAAGp21jFAA")
 
     # Channel ID where the bot searches for content
     CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001234567890"))
